c9c63dd02478197e8bfa85f6d5bdba7a98a284d5
masayahak <masayahak@gmail.com>
2025-05-25 06:16:46 +0900

    プロジェクト ファイルを追加します。

---
+++ CustomTextbox/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CustomTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button検証 = new Button();
+            VTextBox氏名 = new ValidatingTextBox();
+            VTextBox氏名カナ = new ValidatingTextBox();
+            VTextBoxメールアドレス = new ValidatingTextBox();
+            ErrorProvider = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)ErrorProvider).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(104, 108);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 3;
+            label1.Text = "氏名";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(104, 158);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 15);
+            label2.TabIndex = 4;
+            label2.Text = "氏名（カナ）";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(104, 215);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 15);
+            label3.TabIndex = 5;
+            label3.Text = "メールアドレス";
+            // 
+            // button検証
+            // 
+            button検証.BackColor = Color.FromArgb(192, 192, 255);
+            button検証.FlatAppearance.BorderSize = 0;
+            button検証.FlatStyle = FlatStyle.Flat;
+            button検証.Location = new Point(196, 276);
+            button検証.Name = "button検証";
+            button検証.Size = new Size(113, 51);
+            button検証.TabIndex = 6;
+            button検証.TabStop = false;
+            button検証.Text = "検証";
+            button検証.UseVisualStyleBackColor = false;
+            // 
+            // VTextBox氏名
+            // 
+            VTextBox氏名.BorderStyle = BorderStyle.FixedSingle;
+            VTextBox氏名.Location = new Point(196, 100);
+            VTextBox氏名.Name = "VTextBox氏名";
+            VTextBox氏名.PlaceholderText = "10文字以内";
+            VTextBox氏名.Required = true;
+            VTextBox氏名.Size = new Size(194, 23);
+            VTextBox氏名.TabIndex = 7;
+            VTextBox氏名.MaxLength = 10;
+            // 
+            // VTextBox氏名カナ
+            // 
+            VTextBox氏名カナ.BorderStyle = BorderStyle.FixedSingle;
+            VTextBox氏名カナ.Location = new Point(196, 156);
+            VTextBox氏名カナ.Name = "VTextBox氏名カナ";
+            VTextBox氏名カナ.PlaceholderText = "10文字以内";
+            VTextBox氏名カナ.Required = true;
+            VTextBox氏名カナ.Size = new Size(194, 23);
+            VTextBox氏名カナ.TabIndex = 8;
+            VTextBox氏名カナ.MaxLength = 10;
+            // 
+            // VTextBoxメールアドレス
+            // 
+            VTextBoxメールアドレス.BorderStyle = BorderStyle.FixedSingle;
+            VTextBoxメールアドレス.Location = new Point(196, 213);
+            VTextBoxメールアドレス.Name = "VTextBoxメールアドレス";
+            VTextBoxメールアドレス.Required = true;
+            VTextBoxメールアドレス.Size = new Size(194, 23);
+            VTextBoxメールアドレス.TabIndex = 9;
+            // 
+            // ErrorProvider
+            // 
+            ErrorProvider.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(VTextBoxメールアドレス);
+            Controls.Add(VTextBox氏名カナ);
+            Controls.Add(VTextBox氏名);
+            Controls.Add(button検証);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)ErrorProvider).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button検証;
+        private ValidatingTextBox VTextBox氏名;
+        private ValidatingTextBox VTextBox氏名カナ;
+        private ValidatingTextBox VTextBoxメールアドレス;
+        private ErrorProvider ErrorProvider;
+    }
+}
---
+++ CustomTextbox/ValidatingTextBox.cs
@@ -0,0 +1,148 @@
+﻿using System.ComponentModel;
+
+namespace CustomTextbox
+{
+    /// <summary>
+    /// 基本仕様：
+    /// １．フォーカスを受けたときボーダーラインを強調し青い太枠へ変える
+    /// ２．フォーカスを失った時にボーダーラインを元に戻す
+    /// ３．フォーカスを受けたとき、文字が入力されていたら全選択する
+    /// ４．エンターキーで次のタブインデックスへ遷移する
+    /// ５．そのテキストボックスの入力値のチェックメソッドを受け取れる。また、文字列の最大長をプロパティに持つ。
+    /// ６．チェックメソッドや文字列長に違反した場合、フォーカスを失った時にボーダーラインを太い赤色にする
+    /// </summary>
+    public class ValidatingTextBox : TextBox
+    {
+        [Category("データ")]
+        [Description("必須項目はTrueにしてください")]
+        [DefaultValue(false)]
+        public bool Required { get; set; } = false;
+        
+        [Category("外観")]
+        [Description("外枠の通常色です")]
+        [DefaultValue(typeof(Color), "Gray")]
+        public Color 通常色 { get; set; } = Color.Gray;
+
+        [Category("外観")]
+        [Description("フォーカスを受けた時の外枠の色です")]
+        [DefaultValue(typeof(Color), "DeepSkyBlue")]
+        public Color フォーカス色 { get; set; } = Color.DeepSkyBlue;
+
+        [Category("外観")]
+        [Description("エラー時の外枠の色です")]
+        [DefaultValue(typeof(Color), "Red")]
+        public Color エラー色 { get; set; } = Color.Red;
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public ErrorProvider? ErrorProvider { get; set; }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Func<string, (bool isValid, string errorMessage)>? Validator { get; set; }
+
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public bool IsValid => _isValid;
+
+        private bool _hasFocus = false;
+        private bool _isValid = true;
+
+        public ValidatingTextBox()
+        {
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            _hasFocus = true;
+            Invalidate();
+
+            if (!string.IsNullOrEmpty(this.Text))
+            {
+                // ３．フォーカスを受けたとき、文字が入力されていたら全選択する
+                this.SelectAll(); // 全選択
+            }
+
+            base.OnEnter(e);
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            _hasFocus = false;
+            _isValid = ValidateInput();
+            Invalidate();
+            base.OnLeave(e);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // ４．エンターキーで次のタブインデックスへ遷移する
+                this.Parent?.SelectNextControl(this, true, true, true, true);
+                e.Handled = true;
+            }
+
+            base.OnKeyDown(e);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            const int WM_PAINT = 0x000F;
+            if (m.Msg == WM_PAINT)
+            {
+                using Graphics g = this.CreateGraphics();
+                // １．フォーカスを受けたときボーダーラインを強調し青い太枠へ変える
+                Color borderColor = 通常色;
+
+                // ２．フォーカスを失った時にボーダーラインを元に戻す
+                if (_hasFocus)
+                    borderColor = フォーカス色;
+                // ６．チェックメソッドや文字列長に違反した場合、フォーカスを失った時にボーダーラインを太い赤色にする
+                else if (!_isValid)
+                    borderColor = エラー色;
+
+                using Pen pen = new Pen(borderColor, 2);
+                Rectangle rect = new Rectangle(0, 0, this.Width - 1, this.Height - 1);
+                g.DrawRectangle(pen, rect);
+            }
+        }
+
+        private bool ValidateInput()
+        {
+            // ５．そのテキストボックスの入力値のチェックメソッドを受け取れる。また、文字列の最大長をプロパティに持つ。
+            var errorMessage = string.Empty;
+
+            if (Required && string.IsNullOrWhiteSpace(this.Text))
+            {
+                errorMessage = "必須入力です。";
+                ErrorProvider?.SetError(this, errorMessage);
+                return false;
+            }
+
+            if (this.Text.Length > MaxLength)
+            {
+                errorMessage = $"最大文字数({MaxLength})を超えています。";
+                ErrorProvider?.SetError(this, errorMessage);
+                return false;
+            }
+
+            if (Validator != null)
+            {
+                var result = Validator(this.Text);
+                if (!result.isValid)
+                {
+                    errorMessage = result.errorMessage;
+                    ErrorProvider?.SetError(this, errorMessage);
+                    return false;
+                }
+            }
+
+            ErrorProvider?.SetError(this, string.Empty);
+            return true;
+        }
+    }
+}
